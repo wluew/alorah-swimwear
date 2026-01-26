0ca903fbc9b5068102e4c117d1228527b64817b5
William <william@antigravity.local>
2026-01-26 08:11:51 -0400

    Fix broken images in About page by using local assets instead of expired Instagram URLs

--- web/src/pages/About.jsx
+++ web/src/pages/About.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './About.css';
 import Reveal from '../components/Reveal';
 
-// Imágenes de Instagram para la página About
-const aboutImages = [
-    'https://scontent-mia3-3.cdninstagram.com/v/t51.82787-15/611738246_17992441628866455_549189247326767495_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=108&ig_cache_key=MzgwNzgwMTk1MTQ4OTI2ODk3MzE3OTkyNDQxNjIyODY2NDU1.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjUxMHg5MDIuc2RyLkMzIn0%3D&_nc_ohc=Byn585kOMEUQ7kNvwFw5KB0&_nc_oc=AdlGQU9du5RxjkjC4jZpgIkHn_96zEEPDwCB0V2NJAk6yio0Wb5OsK6-hPllf6Cmlqg&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-mia3-3.cdninstagram.com&_nc_gid=4uYKoQerF9SHpuZV0IVK9A&oh=00_AfqNQYxjpZy3tLAJqwekFNQc3HPBRMot-Me_Gl2vOMhZRA&oe=6979426F',
-    'https://scontent-mia3-2.cdninstagram.com/v/t51.82787-15/619906379_17993459723866455_5725686459540607372_n.jpg?stp=dst-jpegr_e35_tt6&_nc_cat=105&ig_cache_key=MzgxNTEyOTc2MzA4MjU3Mjc4OA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTQ0MC5oZHIuQzMifQ%3D%3D&_nc_ohc=vgtMyCg2WlMQ7kNvwG1HRlI&_nc_oc=Adn3eVZZGUGv6wZ5hNT_XfQJpVhyv6x7Ihuguu7IAF0TWE9JvfNcMMLT98dZpYTSsFU&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-mia3-2.cdninstagram.com&_nc_gid=4uYKoQerF9SHpuZV0IVK9A&oh=00_AfpKerDIm84s49bt85sdq8t1t5m4Kvx9X5uXCwj72uKLuA&oe=6979569A',
-    'https://scontent-mia5-1.cdninstagram.com/v/t51.82787-15/618643964_17993275811866455_9154089267648411217_n.jpg?stp=dst-jpegr_e35_tt6&_nc_cat=102&ig_cache_key=MzgxMzczMjczMzQ2NjY4MDg2Ng%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTQ0MC5oZHIuQzMifQ%3D%3D&_nc_ohc=Y4Xqgnq8-80Q7kNvwFPkbSF&_nc_oc=AdlQZSipzblQcxQSHuaaOcA8nN1dyi2z2WzmJ_wuMwIvNQYsz3fpQ_h6I7AVy009xMA&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-mia5-1.cdninstagram.com&_nc_gid=4uYKoQerF9SHpuZV0IVK9A&oh=00_AfqPLZD1-4FAvUM5ZX6FclPim2o2CIaGf28-zAivJALG7A&oe=6979642D'
-];
+import ig1 from '../assets/ig1.jpg';
+import ig2 from '../assets/ig2.jpg';
+import ig3 from '../assets/ig3.jpg';
+
+// Imágenes importadas localmente para asegurar carga confiable
+const aboutImages = [ig1, ig2, ig3];
 
 const About = () => {
     return (
